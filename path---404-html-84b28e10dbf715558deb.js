8f8e8cd09686fdc1965563adf8fd5ddf57e1fb91
govindpatel <govind.patel@freshworks.com>
2019-01-16 12:42:30 +0530

    Updates

---
+++ path---404-html-84b28e10dbf715558deb.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xa2868bfb69fc],{400:function(e,t){e.exports={data:{site:{siteMetadata:{title:"Blog by Govind Patel",subtitle:"Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.",copyright:"© All rights reserved.",menu:[{label:"Articles",path:"/"},{label:"About me",path:"/about/"},{label:"Contact me",path:"/contact/"},{label:"One More Menu",path:"/one-more-path/"}],author:{name:"Govind Patel",email:"#",telegram:"#",twitter:"#",github:"#",rss:"#",vk:"#"}}}},pathContext:{}}}});
+//# sourceMappingURL=path---404-html-84b28e10dbf715558deb.js.map
