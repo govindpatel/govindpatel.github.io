d5f5de5d50cdca7de6ae55c8596c89cce871e3c0
govindpatel <govind.patel@freshworks.com>
2019-01-16 15:07:59 +0530

    Updates

---
+++ path---404-06f426166afd77606891.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xe70826b53c04],{406:function(e,t){e.exports={data:{site:{siteMetadata:{title:"Blog by Govind Patel",subtitle:"Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.",copyright:"© All rights reserved.",menu:[{label:"Articles",path:"/"}],author:{name:"Govind Patel",email:"#",telegram:"#",twitter:"#",github:"#",rss:"#",vk:"#"}}}},pathContext:{}}}});
+//# sourceMappingURL=path---404-06f426166afd77606891.js.map
---
+++ path---404-html-06f426166afd77606891.js
@@ -0,0 +1,2 @@
+webpackJsonp([0xa2868bfb69fc],{405:function(e,t){e.exports={data:{site:{siteMetadata:{title:"Blog by Govind Patel",subtitle:"Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.",copyright:"© All rights reserved.",menu:[{label:"Articles",path:"/"}],author:{name:"Govind Patel",email:"#",telegram:"#",twitter:"#",github:"#",rss:"#",vk:"#"}}}},pathContext:{}}}});
+//# sourceMappingURL=path---404-html-06f426166afd77606891.js.map
